b06c2bc95dbdf73be4380355a6779c8559ed2b86
katar <cihan.katar@mevis.fraunhofer.de>
2026-02-11 12:26:15 +0300

    data_loadeer

--- data/data_loader.py
+++ data/data_loader.py
@@ -111,13 +111,13 @@ def data_transform():
     return DinoMultiCropTransform(
         crop_transform_global, color_transform_global,
         crop_transform_local, color_transform_local,
-        n_global=1, n_local=1
+        n_global=2, n_local=4
     )
 
 class DinoMultiCropTransform:
     def __init__(self, crop_transform_global, color_transform_global,
                  crop_transform_local, color_transform_local,
-                 n_global=1, n_local=1):
+                 n_global=2, n_local=4):
         
         self.crop_global = crop_transform_global
         self.color_global = color_transform_global
