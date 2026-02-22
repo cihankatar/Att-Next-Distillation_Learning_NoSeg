a958ecb93910749c3f58942966570d22f8b35fed
katar <cihan.katar@mevis.fraunhofer.de>
2026-02-23 01:04:38 +0300

    noseg300

--- train_dino.py
+++ train_dino.py
@@ -83,7 +83,7 @@ def update_teacher(student, teacher, momentum):
 
 def main():
 
-    data, training_mode, op, dinowithsegloss, startwithcombinedloss = 'isic_2018_1', "ssl", "train",True,True
+    data, training_mode, op, dinowithsegloss, startwithcombinedloss = 'isic_2018_1', "ssl", "train",False,False
 
     best_loss   = float("inf")
     device      = using_device()
